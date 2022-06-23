75715a0e8888c8717e852c08e6331cad27c046fd
Jayven Nhan <nhantrivinh@gmail.com>
2022-06-23 18:59:01 +0800

    Push my weak code

--- react-native-firebase-ml/components/camera/ImagePicker.tsx
+++ react-native-firebase-ml/components/camera/ImagePicker.tsx
@@ -1,8 +1,10 @@
-import { Alert, Button, View } from 'react-native';
+import { Alert, Button, View, Image, Text, StyleSheet } from 'react-native';
 import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
+import { useState } from 'react';
 
 function ImagePicker() {
     const [cameraPermissionInformation, requestPermission] = useCameraPermissions();
+    const [pickedImage, setPickedImage] = useState('');
     async function verifyPermissions() {
         if (cameraPermissionInformation?.status === PermissionStatus.UNDETERMINED) {
             const permissionResponse = await requestPermission();
@@ -22,19 +24,42 @@ function ImagePicker() {
         if (!isPermissible) {
             return;
         }
-        const image = await launchCameraAsync({
+        const result = await launchCameraAsync({
             allowsEditing: true,
             aspect: [16, 9],
             quality: 0.5,
         });
-        console.log(image);
+        if (!result.cancelled) {
+            setPickedImage(result.uri)
+        }
+    }
+    let imagePreview = <Text>No image taken yet.</Text>
+    if (pickedImage) {
+        imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
     }
     return (
         <View>
-            <View></View>
+            <View style={styles.imagePreview}>{imagePreview}</View>
             <Button title='Take Image' onPress={takeImageHandler} />
-        </View>
+        </View >
     );
 };
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+    imagePreview: {
+        resizeMode: 'contain',
+        width: '100%',
+        height: 200,
+        marginVertical: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(153, 204, 255, 1)',
+        borderRadius: 4
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    }
+});
