70b5c92ece16feb34eef4e23744c6d123bd8a57c
Jayven Nhan <nhantrivinh@gmail.com>
2022-06-23 17:36:19 +0800

    Update App.tsx

--- react-native-firebase-ml/App.tsx
+++ react-native-firebase-ml/App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ImagePicker from './components/camera/ImagePicker';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View>
-        <Text>Title</Text>
+        <ImagePicker />
       </View>
     </View>
   );
