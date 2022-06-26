e8417c5297fdc8ee3865468543cc07ecc6bca2b0
Jayven Nhan <nhantrivinh@gmail.com>
2022-06-26 16:51:50 +0800

    Add Google Vision related files

---
+++ react-native-firebase-ml/secrets.js
@@ -0,0 +1,9 @@
+export const FIREBASE_API_KEY = 'AIzaSyBU3PFIWJ45nNOYZ42ozn6-Mw19cVWIxtM'
+export const FIREBASE_AUTH_DOMAIN = 'visiongn-f1eb3.firebaseapp.com'
+export const FIREBASE_PROJECT_ID = 'visiongn-f1eb3'
+
+export const FIREBASE_STORAGE_BUCKET = 'visiongn-f1eb3.appspot.com'
+export const FIREBASE_MESSAGING_SENDER_ID = '950706490237'
+
+export const GOOGLE_CLOUD_API = 'AIzaSyAh7ZBEmP011Vy45aEpfedPt_Zw1VxV-5I'
+
