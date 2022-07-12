c8058d1dc8879ad66018d067cdc58d199dbc9884
Jayven Nhan <nhantrivinh@gmail.com>
2022-07-13 03:43:11 +0800

    Add AppCenter setup code

--- react-native-firebase-ml/ios/reactnativefirebaseml/AppDelegate.mm
+++ react-native-firebase-ml/ios/reactnativefirebaseml/AppDelegate.mm
@@ -8,6 +8,10 @@
 
 #import <React/RCTAppSetupUtils.h>
 
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
+
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
 #import <React/RCTCxxBridgeDelegate.h>
@@ -31,6 +35,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
+  
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [self.reactDelegate createBridgeWithDelegate:self launchOptions:launchOptions];
